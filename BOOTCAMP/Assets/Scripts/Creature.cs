0a26aa502c4741fd085de935db0d94ce51d3680e
hakan.sarioglu <hakan.sarioglu@bites.com.tr>
2023-11-14 15:58:32 +0300

    field of view updates

--- BOOTCAMP/Assets/Scripts/Creature.cs
+++ BOOTCAMP/Assets/Scripts/Creature.cs
@@ -2,5 +2,11 @@ using UnityEngine;
 
 public class Creature : MonoBehaviour
 {
+    public enum Team
+    {
+        Player, Enemy
+    }
 
+    public Team team;
+    public Transform head;
 }
--- BOOTCAMP/Assets/Scripts/FieldOfView.cs
+++ BOOTCAMP/Assets/Scripts/FieldOfView.cs
@@ -1,13 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using UnityEditor;
 using UnityEngine;
 
 public class FieldOfView : MonoBehaviour
 {
+    public List<Transform> visibleObjects = new List<Transform>();
+
     [SerializeField] private Color _gizmoColor = Color.red;
     [SerializeField] private float _viewRadius = 6f;
     [SerializeField] private float _viewAngle = 30f;
+    [SerializeField] private Creature _creature;
+    [SerializeField] private LayerMask _blockedLayers;
 
     // Start is called before the first frame update
     void Start()
@@ -18,25 +23,48 @@ public class FieldOfView : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        visibleObjects.Clear();
         var targetsInViewRadius = Physics.OverlapSphere(transform.position, _viewRadius);
 
         foreach (var target in targetsInViewRadius)
         {
-            if (!target.TryGetComponent(out Creature _)) continue;
+            if (!target.TryGetComponent(out Creature targetCreature)) continue;
 
-            Debug.Log(target);
+            if (_creature.team == targetCreature.team) continue;
+
+            Vector3 directionToTarget = (target.transform.position - transform.position).normalized;
+
+            if(Vector3.Angle(transform.forward, directionToTarget) < _viewAngle)
+            {
+                Vector3 headPos = _creature.head.position;
+                Vector3 targetHeadPos = targetCreature.head.position;
+
+                Vector3 dirToTargetHead = (targetHeadPos - headPos).normalized;
+                float distToTargetHead = Vector3.Distance(headPos, targetHeadPos);
+
+                if(Physics.Raycast(headPos, dirToTargetHead, distToTargetHead, _blockedLayers))
+                {
+                    continue;
+                }
+                Debug.DrawLine(headPos, targetHeadPos, Color.green);
+
+                visibleObjects.Add(target.transform);
+            }
         }
     }
 
     private void OnDrawGizmos()
     {
         Gizmos.color = _gizmoColor;
-        Gizmos.DrawWireSphere(transform.position, _viewRadius);
+        Handles.color = _gizmoColor;
+
+        Handles.DrawWireArc(transform.position, transform.up, transform.forward, _viewAngle, _viewRadius);
+        Handles.DrawWireArc(transform.position, transform.up, transform.forward, -_viewAngle, _viewRadius);
 
         Vector3 lineA = Quaternion.AngleAxis(_viewAngle, transform.up) * transform.forward;
         Vector3 lineB = Quaternion.AngleAxis(-_viewAngle, transform.up) * transform.forward;
-        Gizmos.color = _gizmoColor;
-        Gizmos.DrawLine(transform.position, transform.position + (lineA * _viewRadius));
-        Gizmos.DrawLine(transform.position, transform.position + (lineB * _viewRadius));
+        Handles.DrawLine(transform.position, transform.position + (lineA * _viewRadius));
+        Handles.DrawLine(transform.position, transform.position + (lineB * _viewRadius));
     }
 }
